90db2326b9625be6d1f0fc387c5ea389ea95321e
Naman Arora <namana843@gmail.com>
2026-03-16 09:55:51 -0700

    Create core/logging/__init__.py with LoggingManager and LoggingConfig classes

---
+++ core/logging/__init__.py
@@ -0,0 +1,22 @@
+class LoggingManager:
+    def __init__(self):
+        # Initialize logger
+        pass
+
+    def log(self, message):
+        # Log a message
+        pass
+
+    def setLevel(self, level):
+        # Set log level
+        pass
+
+
+class LoggingConfig:
+    def __init__(self):
+        # Load logging configuration
+        pass
+
+    def configure(self):
+        # Configure logging
+        pass
